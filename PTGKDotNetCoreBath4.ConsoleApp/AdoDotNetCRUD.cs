0369e4b192ae93e0de822d72a992478d538f43f5
PhyoeTheingi Khin <phyokhin869cu@gmail.com>
2024-07-01 23:40:07 +0630

    Add project files.

---
+++ PTGKDotNetCoreBath4.ConsoleApp/AdoDotNetCRUD.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTGKDotNetCoreBath4.ConsoleApp
+{
+    public class AdoDotNetCRUD
+    {
+        public void Read()
+        {
+            SqlConnection connection = new SqlConnection("Data Source=.;Initial Catalog=PractiseDb;User Id=sa;Password=sasa@123");
+            Console.WriteLine("Connection is opening");
+            connection.Open();
+            Console.WriteLine("Connection is opened");
+            string query = "select * from Tbl_Customer";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+            connection.Close();
+            Console.WriteLine("Connection is closed!");
+            foreach (DataRow dr in dt.Rows)
+            {
+                Console.WriteLine("CustomerId=>" + dr["CustomerId"]);
+                Console.WriteLine("CustomerName=>" + dr["CustomerName"]);
+                Console.WriteLine("PhoneNo=>" + dr["PhoneNo"]);
+                Console.WriteLine("Address=>" + dr["Address"]);
+                Console.WriteLine("Gender=>" + dr["Gender"]);
+                Console.WriteLine("CustomerCode=>" + dr["CustomerCode"]);
+                Console.WriteLine("------------------------------------------");
+            }
+        }
+        public void Create(string name, string phoneno, string address, string gender, string code)
+        {
+            SqlConnection connection = new SqlConnection("Data Source=.;Initial Catalog=PractiseDb;User Id=sa;Password=sasa@123");
+            Console.WriteLine("Connection is opening");
+            connection.Open();
+            Console.WriteLine("Connection is opened");
+            string query = @"INSERT INTO [dbo].[Tbl_Customer]
+           ([CustomerName]
+           ,[PhoneNo]
+           ,[Address]
+           ,[Gender]
+           ,[CustomerCode])
+     VALUES
+           (@CustomerName
+           ,@PhoneNo
+           ,@Address
+           ,@Gender
+           ,@CustomerCode)";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@CustomerName", name);
+            cmd.Parameters.AddWithValue("@PhoneNo", phoneno);
+            cmd.Parameters.AddWithValue("@Address", address);
+            cmd.Parameters.AddWithValue("@Gender", gender);
+            cmd.Parameters.AddWithValue("@CustomerCode", code);
+
+            int result = cmd.ExecuteNonQuery();
+            connection.Close();
+            var message = result > 0 ? "Saving Successful!" : "Saving Failed!";
+            Console.WriteLine(message);
+        }
+        public void Update(int id, string name, string phoneno, string address, string gender, string code)
+        {
+            SqlConnection connection = new SqlConnection("Data Source=.;Initial Catalog=PractiseDb;User Id=sa;Password=sasa@123");
+            Console.WriteLine("Connection is opening");
+            connection.Open();
+            string query = @"UPDATE [dbo].[Tbl_Customer]
+            SET [CustomerName] = @CustomerName
+                ,[PhoneNo] =@PhoneNo
+                ,[Address] =@Address
+                ,[Gender] = @Gender
+                ,[CustomerCode] = @CustomerCode
+            WHERE CustomerId=@CustomerId";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@CustomerId", id);
+            cmd.Parameters.AddWithValue("@CustomerName", name);
+            cmd.Parameters.AddWithValue("@PhoneNo", phoneno);
+            cmd.Parameters.AddWithValue("@Address", address);
+            cmd.Parameters.AddWithValue("@Gender", gender);
+            cmd.Parameters.AddWithValue("@CustomerCode", code);
+            int result = cmd.ExecuteNonQuery();
+            connection.Close();
+            var message = result > 0 ? "Updating Successful!" : "Updadting Failed!";
+            Console.WriteLine(message);
+        }
+        public void Delete(int id)
+        {
+            SqlConnection connection = new SqlConnection("Data Source=.;Initial Catalog=PractiseDb;User Id=sa;Password=sasa@123");
+            connection.Open();
+            Console.WriteLine("Connection is opened");
+            string query = @"DELETE FROM [dbo].[Tbl_Customer]
+             WHERE CustomerId=@CustomerId";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@CustomerId", id);
+            int result = cmd.ExecuteNonQuery();
+            connection.Close();
+            var message = result > 0 ? "Deleting Successful!" : "Deleting Failed!";
+            Console.WriteLine(message);
+        }
+        public void Edit(int id)
+        {
+            SqlConnection connection = new SqlConnection("Data Source=.;Initial Catalog=PractiseDb;User Id=sa;Password=sasa@123");
+            connection.Open();
+            string query = "select * from Tbl_Customer where CustomerId=@CustomerId";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@CustomerId", id);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+            connection.Close();
+            Console.WriteLine("Connection is closed!");
+            if (dt.Rows.Count == 0)
+            {
+                Console.WriteLine("No data found");
+                return;
+            }
+            DataRow dr = dt.Rows[0];
+            Console.WriteLine("CustomerId=>" + dr["CustomerId"]);
+            Console.WriteLine("CustomerName=>" + dr["CustomerName"]);
+            Console.WriteLine("PhoneNo=>" + dr["PhoneNo"]);
+            Console.WriteLine("Address=>" + dr["Address"]);
+            Console.WriteLine("Gender=>" + dr["Gender"]);
+            Console.WriteLine("CustomerCode=>" + dr["CustomerCode"]);
+            Console.WriteLine("------------------------------------------");
+
+
+        }
+    }
+}
---
+++ PTGKDotNetCoreBath4.ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PTGKDotNetCoreBath4.ConsoleApp;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+Console.WriteLine("Hello, World!");
+AdoDotNetCRUD adoDotNetCRUD = new AdoDotNetCRUD();
+//adoDotNetCRUD.Read();
+//adoDotNetCRUD.Create("Aye Aye","09364522868","Yangon","Female","C000230");
+//adoDotNetCRUD.Read();
+//adoDotNetCRUD.Update(3, "Aye Aye", "09364522868", "Yangon", "Female", "C000230");
+//adoDotNetCRUD.Update(333, "Aye Aye", "09364522868", "Yangon", "Female", "C000230");
+adoDotNetCRUD.Delete(1);
+adoDotNetCRUD.Delete(72);
+Console.ReadKey();
+
