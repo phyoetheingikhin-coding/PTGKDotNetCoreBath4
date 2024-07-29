f02a9af640e692fbd3be0be0096db3603abd67f8
PhyoeTheingi Khin <phyokhin869cu@gmail.com>
2024-07-30 00:15:55 +0630

    feat : add result pattern

--- PTGKDotNetCoreBath4.WebApi/Shared/Result.cs
+++ PTGKDotNetCoreBath4.WebApi/Shared/Result.cs
@@ -2,5 +2,20 @@
 {
     public class Result
     {
+        public Result(bool isSuccess, Error error)
+        {
+           IsSuccess = isSuccess;
+            Error = error;
+        }
+
+        public bool IsSuccess { get; set; }
+        public Error Error { get; set; }
+
+        public static Result Success() => new (true, Error.None);
+        public static Result Failure(Error error) => new (false,  error);
+        public static Result <T> Success <T>(T data) => new (true, Error.None,data);
+
+        public static Result <T> Failure <T>(Error error) => new(false, error, default);
+
     }
 }
